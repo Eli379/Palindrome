d42ffb2482d342ced9e8dec5bd2227853432a567
Elijah <elisushi44@gmail.com>
2023-07-24 01:46:20 +1000

    Handles regex and added documentation

--- Palindrome.js
+++ Palindrome.js
@@ -1,12 +1,27 @@
-function palindrome(str){
+/**
+ * Check if a given string is a palindrome.
+ *
+ * @param {string} str - The input string to check for palindrome.
+ * @returns {boolean} Returns true if the input string is a palindrome, otherwise false.
+ *
+ * @example
+ * palindrome("eye"); // Output: true
+ * palindrome("hello"); // Output: false
+ * palindrome("A man, a plan, a canal, Panama"); // Output: true
+ */
+function palindrome(str) {
+    // Regular expression to remove non-alphanumeric characters and underscores
+    var regex = /[\W_]/g;
+  
+    // Convert the string to lowercase and remove non-alphanumeric characters
+    str = str.toLowerCase().replace(regex, "");
+  
+    // Reverse the string
     var reversed = str.split("").reverse().join("");
-    if (reversed === str){
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-console.log(palindrome("eye"));
+  
+    // Check if the reversed string is equal to the original string
+    return reversed === str;
+  }
+  
+  // Example usage
+  console.log(palindrome("eye")); // Output: true
